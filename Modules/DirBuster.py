3b85c155f6c36b79c5cac997630830e83662fddf
Lucas Bader <lcs.bader@googlemail.com>
2015-05-22 15:16:55 +0200

    fixed intendation

--- Modules/DirBuster.py
+++ Modules/DirBuster.py
@@ -2,6 +2,10 @@
 from WrapMapModule import WrapMapModule
 import subprocess
 
+# OPTIONS
+# SSL: Boolean: Use SSL for dirb scan (optional)
+# WORDLIST: String: Wordlist for dirb scan (optional) 
+
 # DirBuster class to enumerate HTTP Servers
 class DirBuster(WrapMapModule):
         
@@ -13,6 +17,7 @@ class DirBuster(WrapMapModule):
         dirb_cmd = ""
         
         print "+ Starting dirb scan against %s on port %s" % (host, port)
+        print "+ SSL: %s" % (self.options['SSL'])
         
         if self.options != None and 'SSL' in self.options.keys() and self.options['SSL'] == True:
             dirb_cmd = "dirb https://%s:%s" % (host, port)
